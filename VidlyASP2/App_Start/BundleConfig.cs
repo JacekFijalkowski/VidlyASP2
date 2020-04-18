3a106e951ace2f0fc57147a74592e075f32cf8ce
Ja Folo <jac.fijalkowski@gmail.com>
2020-04-18 13:16:49 +0200

    Custom Validation of Customer

--- VidlyASP2/App_Start/BundleConfig.cs
+++ VidlyASP2/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace VidlyASP2
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
---
+++ VidlyASP2/Models/Min18YearsIfAMember.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace VidlyASP2.Models
+{
+    public class Min18YearsIfAMember : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var customer = (Customer)validationContext.ObjectInstance;
+
+            if (customer.MembershipTypeId == 0 || customer.MembershipTypeId == 1)
+            {
+                return ValidationResult.Success;
+            }
+
+            if (customer.Birthdate == null)
+            {
+                return new ValidationResult("Birthday is requied.");
+            }
+
+            var age = DateTime.Today.Year - customer.Birthdate.Value.Year;
+
+            return (age >= 18) 
+                ? ValidationResult.Success 
+                : new ValidationResult("Too young for this membership!");
+
+
+        }
+    }
+}
